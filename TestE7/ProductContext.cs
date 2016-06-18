b90d82d468092d25c43e70e23215f22dfaee03df
pontoreau <pontoreau.sylvain@gmail.com>
2016-06-18 17:22:06 +0200

    Litte entity manipulation

---
+++ TestE7/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+
+namespace TestE7
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options)
+        : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
---
+++ TestProgram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Data.Entity;
+using TestE7;
+
+namespace TestProgram
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ProductContext>();
+            optionsBuilder.UseInMemoryDatabase();
+
+            using (var context = new ProductContext(optionsBuilder.Options))
+            {
+                context.Products.Add(new Product
+                {
+                    Name = "Test"
+                });
+                context.SaveChanges();
+
+                var product = context.Products.Single(x => x.Id == 1);
+
+                Console.WriteLine($"{product.Name}");
+
+                product.Name = "Change";
+                context.SaveChanges();
+
+                var changeProduct = context.Products.Single(x => x.Id == 1);
+
+                Console.WriteLine($"{changeProduct.Name}");
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
